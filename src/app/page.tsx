dc6302c77b276ec07c18935595ad136f0ea3128c
Hullus <73313334+Hullus@users.noreply.github.com>
2025-06-06 19:56:39 +0400

    Feat: Metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
 import DeviBirthdayCountdown from "./components/DeviBirthdayCountdown";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: 'დევიკოს დაბადების დღე',
+    description: 'ოფიციალური საიტი დევიკოს დაბადებისდღის.',
+};
 
 export default function Home() {
     return <DeviBirthdayCountdown/>;
